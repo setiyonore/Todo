34ba9ceff2b6a73d69e7eb2823d4d78aaf1c1737
stynore <setiyono.ressly@gmail.com>
2022-03-18 20:53:20 +0700

    Add project files.

---
+++ Todk/ViewModels/ViewModelBase.cs
@@ -0,0 +1,11 @@
+﻿using ReactiveUI;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Todk.ViewModels
+{
+    public class ViewModelBase : ReactiveObject
+    {
+    }
+}
---
+++ Todk/Views/MainWindow.axaml.cs
@@ -0,0 +1,12 @@
+using Avalonia.Controls;
+
+namespace Todk.Views
+{
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+    }
+}
